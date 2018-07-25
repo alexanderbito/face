97131cd2ffdaa27fb165995e60985627b36b030f
hai <phanngochai1008@gmail.com>
2018-07-25 16:55:41 +0700

    hai site

--- faceRecog/urls.py
+++ faceRecog/urls.py
@@ -22,9 +22,9 @@ urlpatterns = [
     url(r'^error_image$', app_views.errorImg),
     url(r'^create_dataset$', app_views.create_dataset),
     url(r'^trainer$', app_views.trainer),
-    url(r'^eigen_train$', app_views.eigenTrain),
+    
     url(r'^detect$', app_views.detect),
-    url(r'^detect_image$', app_views.detectImage),
+    
     url(r'^admin/', admin.site.urls),
     url(r'^records/', include('records.urls')),
 ]
--- faceRecog/views.py
+++ faceRecog/views.py
@@ -4,17 +4,13 @@ import sqlite3
 from django.shortcuts import redirect
 import numpy as np
 import logging
-from sklearn.model_selection import train_test_split
+
 from . import dataset_fetch as df
 from . import cascade as casc
 from PIL import Image
 
 from time import time
-from sklearn.decomposition import PCA
-from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+
 import matplotlib.pyplot as plt
 import pickle
 
@@ -213,146 +209,4 @@ def detect(request):
     cv2.destroyAllWindows()
     return redirect('/')
 
-def eigenTrain(request):
-    path = BASE_DIR+'/ml/dataset'
 
-    # Fetching training and testing dataset along with their image resolution(h,w)
-    ids, faces, h, w= df.getImagesWithID(path)
-    print 'features'+str(faces.shape[1])
-    # Spliting training and testing dataset
-    X_train, X_test, y_train, y_test = train_test_split(faces, ids, test_size=0.25, random_state=42)
-    #print ">>>>>>>>>>>>>>> "+str(y_test.size)
-    n_classes = y_test.size
-    target_names = ['Manjil Tamang', 'Marina Tamang','Anmol Chalise']
-    n_components = 15
-    print("Extracting the top %d eigenfaces from %d faces"
-          % (n_components, X_train.shape[0]))
-    t0 = time()
-
-    pca = PCA(n_components=n_components, svd_solver='randomized',whiten=True).fit(X_train)
-
-    print("done in %0.3fs" % (time() - t0))
-    eigenfaces = pca.components_.reshape((n_components, h, w))
-    print("Projecting the input data on the eigenfaces orthonormal basis")
-    t0 = time()
-    X_train_pca = pca.transform(X_train)
-    X_test_pca = pca.transform(X_test)
-    print("done in %0.3fs" % (time() - t0))
-
-    # #############################################################################
-    # Train a SVM classification model
-
-    print("Fitting the classifier to the training set")
-    t0 = time()
-    param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-                  'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
-    clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
-    clf = clf.fit(X_train_pca, y_train)
-    print("done in %0.3fs" % (time() - t0))
-    print("Best estimator found by grid search:")
-    print(clf.best_estimator_)
-
-    # #############################################################################
-    # Quantitative evaluation of the model quality on the test set
-
-    print("Predicting people's names on the test set")
-    t0 = time()
-    y_pred = clf.predict(X_test_pca)
-    print("Predicted labels: ",y_pred)
-    print("done in %0.3fs" % (time() - t0))
-
-    print(classification_report(y_test, y_pred, target_names=target_names))
-    # print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
-
-    # #############################################################################
-    # Qualitative evaluation of the predictions using matplotlib
-
-    def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
-        """Helper function to plot a gallery of portraits"""
-        plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
-        plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
-        for i in range(n_row * n_col):
-            plt.subplot(n_row, n_col, i + 1)
-            plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
-            plt.title(titles[i], size=12)
-            plt.xticks(())
-            plt.yticks(())
-
-    # plot the gallery of the most significative eigenfaces
-    eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
-    plot_gallery(eigenfaces, eigenface_titles, h, w)
-    # plt.show()
-
-    '''
-        -- Saving classifier state with pickle
-    '''
-    svm_pkl_filename = BASE_DIR+'/ml/serializer/svm_classifier.pkl'
-    # Open the file to save as pkl file
-    svm_model_pkl = open(svm_pkl_filename, 'wb')
-    pickle.dump(clf, svm_model_pkl)
-    # Close the pickle instances
-    svm_model_pkl.close()
-
-
-
-    pca_pkl_filename = BASE_DIR+'/ml/serializer/pca_state.pkl'
-    # Open the file to save as pkl file
-    pca_pkl = open(pca_pkl_filename, 'wb')
-    pickle.dump(pca, pca_pkl)
-    # Close the pickle instances
-    pca_pkl.close()
-
-    plt.show()
-
-    return redirect('/')
-
-
-def detectImage(request):
-    userImage = request.FILES['userImage']
-
-    svm_pkl_filename =  BASE_DIR+'/ml/serializer/svm_classifier.pkl'
-
-    svm_model_pkl = open(svm_pkl_filename, 'rb')
-    svm_model = pickle.load(svm_model_pkl)
-    #print "Loaded SVM model :: ", svm_model
-
-    pca_pkl_filename =  BASE_DIR+'/ml/serializer/pca_state.pkl'
-
-    pca_model_pkl = open(pca_pkl_filename, 'rb')
-    pca = pickle.load(pca_model_pkl)
-    #print pca
-
-    '''
-    First Save image as cv2.imread only accepts path
-    '''
-    im = Image.open(userImage)
-    #im.show()
-    imgPath = BASE_DIR+'/ml/uploadedImages/'+str(userImage)
-    im.save(imgPath, 'JPEG')
-
-    '''
-    Input Image
-    '''
-    try:
-        inputImg = casc.facecrop(imgPath)
-        inputImg.show()
-    except :
-        print("No face detected, or image not recognized")
-        return redirect('/error_image')
-
-    imgNp = np.array(inputImg, 'uint8')
-    #Converting 2D array into 1D
-    imgFlatten = imgNp.flatten()
-    #print imgFlatten
-    #print imgNp
-    imgArrTwoD = []
-    imgArrTwoD.append(imgFlatten)
-    # Applyting pca
-    img_pca = pca.transform(imgArrTwoD)
-    #print img_pca
-
-    pred = svm_model.predict(img_pca)
-    print(svm_model.best_estimator_)
-    print pred[0]
-
-    return redirect('/records/details/'+str(pred[0]))
